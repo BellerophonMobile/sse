7ab538ea013e46eecbac5c06c84c47d84cc0f11b
Joe Kopena <tjkopena@bellerophonmobile.com>
2016-12-14 11:30:33 -0500

    History.

--- EventServer.go
+++ EventServer.go
@@ -7,34 +7,80 @@ import (
 	"log"
 )
 
+type record struct {
+	id string
+	event string
+	data string
+	next *record
+}
+
+type history struct {
+	head *record
+	tail *record
+	limit int
+}
+
+func (x *history) push(id, event, data string) {
+
+	rec := &record{
+		id: id,
+		event: event,
+		data: data,
+	}
+
+	if x.tail != nil {
+		x.tail.next = rec
+	}
+	x.tail = rec
+	
+	if x.head != nil {
+		if x.limit <= 0 {
+			x.head = x.head.next
+		} else {
+			x.limit--
+		}
+	} else {
+		x.head = rec
+	}
+
+}
+
 type EventServerOptions struct {
 	ActionQueueLength int
 	LogLabel string
 	RetryMillis int
+	HistoryLimit int
 }
+
 
 type EventServer struct {
 	label string
 	actions chan func()
 	connections map[*Writer]*Writer
+	backlog *history
 	retrymillis int
 }
 
 func NewEventServer(opts *EventServerOptions) *EventServer {
+
 	if opts == nil {
-		opts = &EventServerOptions{
-		}
+		opts = &EventServerOptions{}
 	}
 
 	if opts.LogLabel == "" {
 		opts.LogLabel = "sse"
 	}
-	
+
+	if opts.HistoryLimit == 0 {
+		opts.HistoryLimit = 100
+	}
+
 	x := &EventServer{
 		label: opts.LogLabel,
 		actions: make(chan func(), opts.ActionQueueLength),
 		connections: make(map[*Writer]*Writer),
 		retrymillis: opts.RetryMillis,
+		backlog: &history{limit: opts.HistoryLimit-1},
 	}
 
 	log.Printf("[%v] New EventServer", x.label)
@@ -63,6 +109,10 @@ func (x *EventServer) Handle(w http.ResponseWriter, r *http.Request) {
 	x.actions <- func() {
 		log.Printf("[%v] Attach", x.label)
 		x.connections[conn] = conn
+
+		for e := x.backlog.head; e != nil; e = e.next {
+			conn.Event(e.id, e.event, e.data)
+		}
 	}
 
 	notify := w.(http.CloseNotifier).CloseNotify()
@@ -83,6 +133,8 @@ func (x *EventServer) Event(id, event, msg string) {
 
 	x.actions <- func() {
 		// log.Printf("[%v] Message '%v': '%v'", x.label, event, msg)
+		x.backlog.push(id, event, msg)
+		
 		for writer := range(x.connections) {
 			_, err := writer.Event(id, event, msg)
 			if err != nil {
